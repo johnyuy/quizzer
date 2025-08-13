7c8dc3db335071f8052a6427b5a5d9b530f957fc
John Yu <57468127+johnyuy@users.noreply.github.com>
2025-08-14 01:52:17 +0800

    Update doc_management.py

--- pages/doc_management.py
+++ pages/doc_management.py
@@ -1,37 +1,37 @@
 import streamlit as st
 import os
-from utility import get_side_bar
+from utility import get_side_bar, check_credentials
 from service.file_processor import process_file
-st.set_page_config(initial_sidebar_state='expanded')
 
+def doc_panel():
+    with st.container(gap='small'):
+        st.title('Upload Document')
 
-with st.container(gap='small'):
-    st.title('Upload Document')
+    to_generate = False
+    st.caption('This document will be used to generate quizzes.')
 
-to_generate = False
+    uploaded_file = st.file_uploader("Choose a file") # TODO add filter for file extension types
+    if uploaded_file is not None:
+        file_path = os.path.abspath(uploaded_file.name)
+        print(f'selected: {file_path}')
+        with st.container(horizontal_alignment='center'):
+            if st.button("Generate Questions", type='primary'):
+                to_generate = True
 
-st.caption('This document will be used to generate quizzes.')
+    if to_generate is True:
+        # TODO call function to process file
+        print('calling file processor to process file')
+        process_file("test")
+        with st.container(horizontal_alignment='center'):
+            st.write("<div style='text-align: center'>success</div>", unsafe_allow_html=True)
 
+    st.divider()
+    st.subheader('Loaded Documents')
+    st.caption('List of validated and processed document list should appear here')
 
-uploaded_file = st.file_uploader("Choose a file") # TODO add filter for file extension types
-if uploaded_file is not None:
-    file_path = os.path.abspath(uploaded_file.name)
-    print(f'selected: {file_path}')
-    with st.container(horizontal_alignment='center'):
-        if st.button("Generate Questions", type='primary'):
-            to_generate = True
+if not check_credentials():  
+    st.stop()
 
-
-if to_generate is True:
-    # TODO call function to process file
-    print('calling file processor to process file')
-    process_file("test")
-    with st.container(horizontal_alignment='center'):
-        st.write("<div style='text-align: center'>success</div>", unsafe_allow_html=True)
-
-st.divider()
-st.subheader('Loaded Documents')
-st.caption('List of validated and processed document list should appear here')
-
-
-get_side_bar()              
+if st.session_state["role"] == "admin":
+    doc_panel()
+    get_side_bar()              
